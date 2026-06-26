a39c870ed37914c2c3a4c853d99615eb7b049585
Rellot's screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2026-06-26 20:48:41 +0530

    added documentation to three classes

--- src/include/dectargs.h
+++ src/include/dectargs.h
@@ -9,8 +9,24 @@
 #include "listSite.h"
 */
 
+/**
+ * This class handles the incoming calls of commandline arguements
+ * if no arguements are given, then this class calls the TUI frontend
+ */
 class dectargs {
 public:
+  /**
+   * This constructor takes a vector of arguements and selects the appropriate destination
+   * 
+   * @param vects 
+   * a string vector containing the main operation and then it's subcommands
+   * 
+   * @example
+   * the vector must statisfy at least one condition, mode, 
+   * ```cpp
+   * dectargs({"add", "test@gmail.com", "website.io"});
+   * ```
+   */
   dectargs(std::vector<std::string> &vects) : mArgVect(vects) {
     mArgc = vects.size();
     checkArgs();
@@ -18,11 +34,50 @@ public:
 
 private:
   int mArgc;
+  /**
+   * max no of arguements that a mode can have
+   */
   int noOfArgsDetect = 2;
+
   std::vector<std::string> mArgVect;
+  /**
+   * compares the first commandline arguement to given parameter
+   * 
+   * @param modeName 
+   * a string containing an operation
+   * 
+   * @return 
+   * true if it matches, false if it don't match
+   */
   bool funcNameCmp(std::string funcName);
+
+  /**
+   * select the type which the arguement is given
+   * 
+   * @danger 
+   * this function can crash the entire program if no commandline arguements are given 
+   */
   void dectTypeArgs();
+  
+  /**
+   * shields the dectTypeArgs() function if too many or too low arguements are given
+   * 
+   * @info 
+   * this is also where the TUI frontend is gonna be executed when no arguements are given
+   */
   void checkArgs();
+
+  /**
+   * simply prints the help message when putting "help"
+   */
   void helpMessage();
+  
+  /**
+   * check the no of arguement to determine whether 
+   * the user wants to regenerate the password or rename a site link or email
+   * 
+   * @warning 
+   * this function is only used for change arguement
+   */
   void ArgNumberDetectchangeFeat();
 };
--- src/include/genPass.h
+++ src/include/genPass.h
@@ -2,14 +2,29 @@
 
 #include "includes.h"
 
+/**
+ * generates a unique random number
+ */
 class GeneratePass {
 public:
     GeneratePass() = default;
+    /**
+     * This is a wrapper or getter function of genPass() function
+     * 
+     * @return genPass() function
+     */
     std::string getgeneratedPass();
 
 private:
+    /**
+     * this generates a unique string using std::mt19937 using std::random_device
+     * which is the modern C++ alternative
+     * 
+     * @return string of random characters
+     */
     std::string genPass();
     std::random_device rd;  // Seed source
+    // TODO: add captial letters
     std::string passchars = "abcdefghijklmnopqrstuvwxyz123456789!@#$%&*";
     std::string passStr;
     int passLen = 15;
