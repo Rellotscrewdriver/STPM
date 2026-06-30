cd5e94ae0aff7db07674de69a2fcc670f44b689c
Rellot's screwdriver <50903223+Rellotscrewdriver@users.noreply.github.com>
2026-06-30 19:49:19 +0530

    fixed remove operation

--- src/SiteOperations/removeSite.cpp
+++ src/SiteOperations/removeSite.cpp
@@ -4,8 +4,8 @@ void siteOps::removeShit(){
     bool isSiteFound = false;
 
     siteDataNew.erase(
-        std::remove_if(siteDataNew.begin(), siteDataNew.end(), [&isSiteFound](siteObj& obj, const siteOps& c) {
-            bool compare = obj.getEmail() == c.email && obj.getLink() == c.link;
+        std::remove_if(siteDataNew.begin(), siteDataNew.end(), [this, &isSiteFound](siteObj& obj) {
+            bool compare = (obj.getEmail() == this->email) && (obj.getLink() == this->link);
             if(compare){
                 isSiteFound = true;
             }
@@ -13,15 +13,6 @@ void siteOps::removeShit(){
         }), 
         siteDataNew.end()
     );
-
-    // for (auto it = siteData.begin(); it != siteData.end(); it++) {
-    //     if (*it == email && *it == link) {
-    //         // erase() returns the iterator to the next valid element
-    //         it = siteData.erase(it);
-    //         isSiteFound = true;
-    //         break;
-    //     }
-    // }
 
     if(isSiteFound){
         std::cout << "Successfully Removed " << link << " from " << email << "\n";
